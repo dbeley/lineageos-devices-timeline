54f03baf74c9f6e4dcb6a90cced96e61e8a46bce
dbeley <6568955+dbeley@users.noreply.github.com>
2024-07-23 10:03:41 +0200

    Add more fields, add images

--- lineageos_devices_timeline.py
+++ lineageos_devices_timeline.py
@@ -16,34 +16,44 @@ for index, file in enumerate(list_files, 1):
 
     formatted_release_date = list(data.get('release')[0].values())[0] if isinstance(data.get('release'), list) else data.get('release')
     formatted_data = f"{data.get('type')} - {data.get('vendor')} - {data.get('name')} ({formatted_release_date})"
-    print(formatted_data)
     list_data.append({
-                         "type": data.get("type"),
+                         "type": data.get("type").title(),
                          "brand": data.get("vendor"),
                          "model": data.get("name"),
                          "release_date": str(formatted_release_date),
+                         "versions": [str(x) for x in data.get("versions")],
+                         "maintainers": data.get("maintainers"),
+                         "codename": data.get("codename"),
                      })
 
 sorted_list_data = sorted(list_data, key=lambda x: (x['release_date'], x['brand'], x['model']), reverse=True)
-print(sorted_list_data)
 
 header = """
 <thead>
     <tr>
+        <th></th>
         <th>Type</th>
         <th>Brand</th>
         <th>Model</th>
+        <th>Codename</th>
         <th>Release Date</th>
+        <th>Versions</th>
+        <th>Status</th>
     </tr>
 </thead>
 """
 
+# https://wiki.lineageos.org/images/devices/salami.png
 table_data = "<tbody>" + "\n".join([f"""
         <tr>
+        <td><img loading='lazy' width='50' src='https://wiki.lineageos.org/images/devices/{data['codename']}.png' alt=''/></td>
         <td>{data['type']}</td>
         <td>{data['brand']}</td>
-        <td>{data['model']}</td>
+        <td><a href='https://wiki.lineageos.org/devices/{data['codename']}'>{data['model']}</a></td>
+        <td>{data['codename']}</td>
         <td>{data['release_date']}</td>
+        <td>{', '.join(data['versions'])}</td>
+        <td>{'Maintained' if len(data['maintainers']) > 0 else 'Not maintained'}</td>
         </tr>
     """ for data in sorted_list_data]) + "</tbody>\n"
 
